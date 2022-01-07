213c69f2397bf2ef3335a6489aeea492f3fd7828
Daky Chanthavong <daki.litd@gmail.com>
2022-01-08 01:32:29 +0700

    Add quiz part 2

---
+++ basic javascrpt 1/1_Hello.js
@@ -0,0 +1,17 @@
+/**
+ * ກຳນົດໃຫ້
+ */
+
+let firstName = "John"
+let lastName = "Doe"
+let age = 35
+
+/**
+ * ຈົ່ງຂຽນ code ເພື່ອໃຫ້ໄດ້ຜົນຮັບດັ່ງນີ້
+ * 
+ *  ==================================== Display ========================================
+ *  Hello, my name is John Doe.
+ *  I'll be 36 years old next month.
+ */
+
+// coding
---
+++ basic javascrpt 1/4_Sum.js
@@ -0,0 +1,8 @@
+/**
+ * ຈົ່ງຂຽນ function ຫາຜົນລວມຂອງ 2 ຈຳນວນ. (ຂຽນໃນຮູບແບບຂອງ Arrow Function)
+ */
+
+let first = 32
+let second = 55
+
+// coding
